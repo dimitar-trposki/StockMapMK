f2ab25abe7651da876dca5846aaff4ba18498347
KonstantinaMihailova <139159171+KonstantinaMihailova@users.noreply.github.com>
2024-12-24 15:51:54 +0100

    Add files via upload

---
+++ Домашно 3/StockMap/src/test/java/mk/ukim/finki/stockmap/StockMapApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.stockmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
